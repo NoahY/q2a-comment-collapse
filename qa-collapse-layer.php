5b19d211ecfa5b38219a046dc8229ffdd8aededa
NoahY <noahy57@gmail.com>
2011-11-15 12:04:52 +0530

    fixed css

--- qa-collapse-layer.php
+++ qa-collapse-layer.php
@@ -5,19 +5,19 @@
 		{
 			qa_html_theme_base::head_custom();
 			if (qa_opt('collapse_comment_enable')) 
-				$this->output('<style>','.qa-collapsed-comment{display:none}','</style>');
+				$this->output('<style>','.qa-comment-collapsed{display:none} .qa-comment-collapser{font-weight:bold} .qa-comment-collapser .qa-c-item-footer{display:none} .qa-comment-collapser .qa-c-item-link{margin:0 !important}','</style>');
 		}	
 		function c_list($c_list, $class)
 		{
-			qa_error_log($c_list);
 			if (qa_opt('collapse_comment_enable') && count($c_list) > qa_opt('collapse_comment_max_comments')) {
 				foreach ($c_list as $idx => $c_item)
 					if($idx>=qa_opt('collapse_comment_max_comments'))
-						$c_list[$idx]['classes'] = @$c_list[$idx]['classes'].' qa-collapsed-comment';
+						$c_list[$idx]['classes'] = @$c_list[$idx]['classes'].' qa-comment-collapsed';
 						
 				$c_list[] =    array(
 					'title' => str_replace('#',count($c_list)-qa_opt('collapse_comment_max_comments'),qa_opt('collapse_comment_text')),
-					'url' => 'javascript:void(0)" onclick="jQuery(this).parent().siblings().show(200); jQuery(this).parent().hide(200)'
+					'url' => 'javascript:void(0)" onclick="jQuery(this).parent().siblings().show(200); jQuery(this).parent().hide(200)',
+					'classes' => 'qa-comment-collapser'
 				);
 						
 			}
